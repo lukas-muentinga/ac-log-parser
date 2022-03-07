b5586f39af61e822868a59ad7d07b5234d6f4d27
Lukas Müntinga <lukas.muentinga@icloud.com>
2022-03-07 19:23:01 +0100

    Finished v0.9

--- ac_log_parser.py
+++ ac_log_parser.py
@@ -1,17 +1,22 @@
 ######################################################################
 # REGEX PARSER FOR ASSETTO CORSO LOG FILES
 # ----------------------------------------
-#
+# Usage: python3 ac_log_parser.py [log-filepath] [sqlite-filepath]
 ######################################################################
 
-import re, sys
+import re, sys, sqlite3
+from datetime import datetime
 
 LAP_END_FINDER_RE = r"""Dispatching\sTCP\smessage\sto\s(?P<car>\S+)\s\(\d\)\s\[(?P<player>\S+).*\n
                         Car.onLapCompleted\n
-                        LAP\s\S+\s(?P<time>\S+)\n
+                        LAP\s\S+\s(?P<lap_time>\S+)\n
                         SPLIT\sCOUNT:\s(?P<split_count>\S+)\n
                         Dynamic\strack\s(?P<dynamic_track>\S+)\n
                         Result.OnLapCompleted\.\sCuts:\s(?P<cuts>\S+)\n"""
+
+# DB Scheme
+DB_TABLE = 'ac-rounds'
+
 
 if __name__ == '__main__':
 
@@ -28,6 +33,23 @@ if __name__ == '__main__':
         print(f'File {logfile_path} could not be opened.')
         sys.exit(1)
 
+    laps = [m.groupdict() for m in re.finditer(lap_end_finder_pattern, log_content)]
 
-    lap_end_parsed = [m.groupdict() for m in re.finditer(lap_end_finder_pattern, log_content)]
-    print(lap_end_parsed)
+    # Parse datatypes
+    for lap in laps:
+        pt = datetime.strptime(lap['lap_time'], '%-M:%S:%f')
+        lap['lap_time'] = pt.microsecond + ( pt.second * 1000 ) + ( pt.minute * 60000 ) 
+        lap['cuts'] = int(lap['cuts'])
+        lap['split_count'] = int(lap['split_count'])
+
+    # Write laps to sqlite db
+    try:
+        con = sqlite3.connect(sys.argv[2])
+        cur = con.cursor()
+        for lap in laps:
+            cur.execute(f"""INSERT INTO {DB_TABLE} ({lap['player']},{lap['car']},{lap['lap_time']},{lap['cuts']},{lap['split_count']},{lap['dynamic_track']})""")
+    except IOError:
+        print(f'SQLite file {sys.argv[2]} could not be opened.')
+
+
+    
